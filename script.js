cea5a44072e8a4de1aa4b27040dead78e02724cd
ousmane91 <contact@ousmangueye.fr>
2019-04-08 14:10:37 +0200

    exomaj

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //pour afficher une boite de dialogue avec la valeur du champ
 function showname() {
   var elem = document.getElementById('lastname').value;
-  elem.onkeypress = showname;
+  elem.onkeyup = showname;
   alert('lastname');
 }
